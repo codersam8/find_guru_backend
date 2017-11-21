ca3b3c3c4f7a256fca571f1980c13d8fdfa59a2d
sampath surineni <sampath.surineni@oracle.com>
2017-11-21 17:48:24 +0530

    defined a guru model

--- list_guru/models.py
+++ list_guru/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Guru(models.Model):
+    mail_id = models.EmailField()
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    qualification = models.CharField(max_length=10)
+    institute = models.CharField(max_length=60)
+    mobile = models.CharField(max_length=15)
