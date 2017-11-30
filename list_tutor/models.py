4f47f2cf688756bb899746d1203e300a76d19e1c
rtakkala <rtakkala@RTAKKALA-IN.oradev.oraclecorp.com>
2017-11-30 09:50:28 +0530

    added new field location

--- list_tutor/models.py
+++ list_tutor/models.py
@@ -8,3 +8,4 @@ class Guru(models.Model):
     qualification = models.CharField(max_length=10)
     institute = models.CharField(max_length=60)
     mobile = models.CharField(max_length=15)
+    location = models.CharField(max_length=30)
